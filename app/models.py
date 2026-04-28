97b8b7824d4c86fffe1021376fd0839058885da1
Ta6leTkaa <a.dubinin@innopolis.university>
2026-04-28 14:35:45 +0300

    authorization added

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from decimal import Decimal
+
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    login: Mapped[str] = mapped_column(unique=True)
 
 class Wallet(Base):
     __tablename__ = "wallet"
@@ -9,3 +15,4 @@ class Wallet(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     balance: Mapped[Decimal]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from app.api.v1.wallets import router as wallet_router
 from app.api.v1.operations import router as operations_router
+from app.api.v1.users import router as users_router
 from app.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(wallet_router, prefix="/api/v1", tags = ["Wallet"])
 app.include_router(operations_router, prefix="/api/v1", tags = ["Operations"])
+app.include_router(users_router, prefix="/api/v1", tags = ["Users"])
 
 Base.metadata.create_all(bind=engine)
