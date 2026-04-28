d7047166218886879d7180945038aaaf70a2f7d2
Ta6leTkaa <a.dubinin@innopolis.university>
2026-04-28 13:27:33 +0300

    dependency injection added

--- app/service/wallets.py
+++ app/service/wallets.py
@@ -3,40 +3,32 @@ from app.database import SessionLocal
 from app.schemas import CreateWalletRequest
 from app.repository import wallets as wallets_repository
 
-def get_balance(wallet_name: str | None = None):
-    db = SessionLocal()
-    try:
-        if wallet_name is None:
-            wallets = wallets_repository.get_all_wallets(db)
-            return {"total_balance": sum([w.amount for w in wallets])}
+def get_balance(db: Session, wallet_name: str | None = None):
+    if wallet_name is None:
+        wallets = wallets_repository.get_all_wallets(db)
+        return {"total_balance": sum([w.amount for w in wallets])}
 
-        if not wallets_repository.is_wallet_exist(db, wallet_name):
-            raise HTTPException(
-                status_code = 404,
-                detail = f"Wallet {wallet_name} not found."
+    if not wallets_repository.is_wallet_exist(db, wallet_name):
+        raise HTTPException(
+            status_code = 404,
+            detail = f"Wallet {wallet_name} not found."
+        )
+    wallet = wallets_repository.get_wallet_balance_by_name(db, wallet_name)
+    return {"wallet": wallet.name, "balance": wallet.balance}
+
+def create_wallet(db: Session, wallet: CreateWalletRequest):
+    if wallets_repository.is_wallet_exist(db, wallet.name):
+        raise HTTPException(
+            status_code = 400,
+            detail = f"Wallet {wallet.name} already exists."
             )
-        wallet = wallets_repository.get_wallet_balance_by_name(db, wallet_name)
-        return {"wallet": wallet.name, "balance": wallet.balance}
-    finally:
-        db.close()
 
-def create_wallet(wallet: CreateWalletRequest):
-    db = SessionLocal()
-    try:
-        if wallets_repository.is_wallet_exist(db, wallet.name):
-            raise HTTPException(
-                status_code = 400,
-                detail = f"Wallet {wallet.name} already exists."
-                )
+    wallet = wallets_repository.create_wallet(db, wallet.name, wallet.initial_balance)
 
-        wallet = wallets_repository.create_wallet(db, wallet.name, wallet.initial_balance)
+    db.commit()
 
-        db.commit()
-
-        return {
-            "message": "Wallet {wallet.name} successfully.",
-            "wallet": wallet.name,
-            "balance": wallet.balance
-        }
-    finally:
-        db.close()
+    return {
+        "message": "Wallet {wallet.name} successfully.",
+        "wallet": wallet.name,
+        "balance": wallet.balance
+    }
